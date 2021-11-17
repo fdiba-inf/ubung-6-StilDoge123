fb316a1339ee3f1aa206fa799a2d65830caac6da
Stilian <stildoge123@abv.bg>
2021-11-17 21:36:37 +0200

    password1

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int passLength = 0;
+        int passLength;
         int digitCount = 0;
         boolean passValid = true;
-        do{
-            String password = input.next().toString();
-             passLength = password.length();
+        do {
+            passValid = true;
+            String password = input.next();
+            passLength = password.length();
             for (int i = 0; i < passLength; i++) {
-                if(!Character.isLetterOrDigit(password.charAt(i))){
-                    passValid=false;
+                if (!Character.isLetterOrDigit(password.charAt(i))) {
+                    passValid = false;
                     break;
-                }
-                else if(Character.isDigit(password.charAt(i))){
+                } else if (Character.isDigit(password.charAt(i))) {
                     digitCount++;
                 }
             }
-        }while(passValid == false || digitCount <2 || passLength<8);
+        } while (!passValid || digitCount < 2 || passLength < 8);
         System.out.println("Password is valid!");
     }
 }
