6a0e545998e0e08866c577fe82516ceebddd2c94
Stilian <stildoge123@abv.bg>
2021-11-17 18:18:05 +0200

    first try

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,18 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int n = input.nextInt();
+        int temp = n;
+        boolean isPalindrome = false;
+        while(n!=0){
+           int a = n%10;
+           sum = sum*10 + a;
+           n/=10;
+        }
+        if(temp == sum){
+            isPalindrome = true;
+        }
+        System.out.println("Palindrome: " + isPalindrome);
     }
 }
