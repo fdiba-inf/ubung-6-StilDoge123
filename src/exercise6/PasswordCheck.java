66c8d4f79e2c146119259ac3df2c5ae30a93dc66
Stilian <stildoge123@abv.bg>
2021-11-17 21:32:45 +0200

    password

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,22 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int passLength = 0;
+        int digitCount = 0;
+        boolean passValid = true;
+        do{
+            String password = input.next().toString();
+             passLength = password.length();
+            for (int i = 0; i < passLength; i++) {
+                if(!Character.isLetterOrDigit(password.charAt(i))){
+                    passValid=false;
+                    break;
+                }
+                else if(Character.isDigit(password.charAt(i))){
+                    digitCount++;
+                }
+            }
+        }while(passValid == false || digitCount <2 || passLength<8);
+        System.out.println("Password is valid!");
     }
 }
